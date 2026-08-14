568edc614b01ef7d997b0cf572cbfaccfee99d33
Ogunsanya_Iteoluwaleyi <Ogunsanyaiteoluwaleyi@gmail.com>
2026-08-14 22:53:52 +0100

    Add: is-subsequence - submission-20

---
+++ Data Structures & Algorithms/is-subsequence/submission-20.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        if s=="":
+            return True
+        s_index=0
+        for char in t:
+            if char==s[s_index]:
+                s_index+=1
+            if len(s)==s_index:
+                return True
+        else:
+            return False
+
+
+
+        
