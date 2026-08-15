3b79628095d6a4fe1aa20511f06f16aaed7b990c
Ogunsanya_Iteoluwaleyi <Ogunsanyaiteoluwaleyi@gmail.com>
2026-08-15 18:38:06 +0100

    Add: length-of-last-word - submission-32

---
+++ Data Structures & Algorithms/length-of-last-word/submission-32.py
@@ -0,0 +1,14 @@
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        count=0
+        for i in range(len(s)-1,-1,-1):
+
+            if s[i]!=" ":
+                count+=1
+                if s[i-1]==" " or i==0:
+                    return count
+
+
+            
+
+        
